9b282827d8fda6141ae71ac1f43dd906fec644d1
Eddy Löwen <mail@eddyloewen.com>
2020-04-13 16:42:55 +0200

    fixes hash plugins

--- .node/GulpHashPlugin.js
+++ .node/GulpHashPlugin.js
@@ -1,55 +1,18 @@
-import fs from 'fs';
 import path from 'path';
-import crypto from 'crypto';
 import through from 'through2';
 
-const defaultOptions = {
-    name: 'hash-manifest.json',
-    replace: false,
-    versionPattern: '[name]?version=[hash]',
-    publicPath: '',
-};
+import { defaultOptions, addToManifest } from './HashHelpers';
 
-function md5(string) {
-    return crypto.createHash('md5').update(string).digest('hex');
-}
-
-function tryRequire(file) {
-    try {
-        return JSON.parse(fs.readFileSync(file, 'utf-8'));
-    } catch (err) {
-        if (err.code === 'ENOENT') return null;
-        throw err;
-    }
-}
-
-function mkdirpath(dest) {
-    const dir = path.dirname(dest);
-    try {
-        fs.readdirSync(dir);
-    } catch (err) {
-        mkdirpath(dir);
-        fs.mkdirSync(dir);
-    }
-}
-
-function generateVersionString(versionPattern, name, hash) {
-    return versionPattern.replace('[name]', name).replace('[hash]', hash);
-}
-
-export default function (options) {
-    options = Object.assign({}, defaultOptions, options);
+export default function (options = {}) {
+    options = { ...defaultOptions, ...options };
 
     return through.obj((chunk, enc, cb) => {
-        // console.log('through chunk', chunk.contents, chunk.path, chunk.cwd, chunk.base);
+        const chunkPath = chunk.path.replace(chunk.cwd + path.sep, '');
+        if (!chunkPath.includes('.map')) {
+            const fileName = options.formatter(chunkPath.split(path.sep).join('/'));
+            const code = chunk.contents;
 
-        const fileName = chunk.path.replace(chunk.cwd + '/', '').replace(options.publicPath, '');
-
-        if (fileName.indexOf('.map') === -1) {
-            const manifest = tryRequire(options.name) || {};
-            manifest[`${fileName}`] = generateVersionString(options.versionPattern, fileName, md5(chunk.contents));
-            mkdirpath(options.name);
-            fs.writeFileSync(options.name, JSON.stringify(manifest, null, 4), 'utf8');
+            addToManifest(fileName, code, options);
         }
 
         cb(null, chunk);
--- .node/RollupHashPlugin.js
+++ .node/RollupHashPlugin.js
@@ -1,70 +1,19 @@
-import fs from 'fs';
 import path from 'path';
-import crypto from 'crypto';
 
-const defaultOptions = {
-    name: 'hash-manifest.json',
-    replace: false,
-    versionPattern: '[name]?version=[hash]',
-    publicPath: '',
-};
-
-function md5(string) {
-    return crypto.createHash('md5').update(string).digest('hex');
-}
-
-function tryRequire(file) {
-    try {
-        return JSON.parse(fs.readFileSync(file, 'utf-8'));
-    } catch (err) {
-        if (err.code === 'ENOENT') return null;
-        throw err;
-    }
-}
-
-function mkdirpath(dest) {
-    const dir = path.dirname(dest);
-    try {
-        fs.readdirSync(dir);
-    } catch (err) {
-        mkdirpath(dir);
-        fs.mkdirSync(dir);
-    }
-}
-
-function generateVersionString(versionPattern, name, hash) {
-    return versionPattern.replace('[name]', name).replace('[hash]', hash);
-}
-
-function writeHash(bundleName, bundleDir, bundleCode, options) {
-    // console.log('writeHash', bundleName, bundleDir);
-
-    const fileName = `${bundleDir}/${bundleName}`.replace(options.publicPath, '');
-
-    const manifest = tryRequire(options.name) || {};
-    manifest[`${fileName}`] = generateVersionString(options.versionPattern, fileName, md5(bundleCode));
-    mkdirpath(options.name);
-    fs.writeFileSync(options.name, JSON.stringify(manifest, null, 4), 'utf8');
-}
+import { defaultOptions, addToManifest } from './HashHelpers';
 
 export default function hash(options = {}) {
-    options = Object.assign({}, defaultOptions, options);
+    options = { ...defaultOptions, ...options };
 
     return {
         name: 'hash-version-manifest',
-        generateBundle: function (outputOptions, bundle) {
-            // writeHash(
-            //     bundle[Object.keys(bundle)[0]].fileName,
-            //     outputOptions.dir,
-            //     bundle[Object.keys(bundle)[0]].code,
-            //     options,
-            // );
-        },
         renderChunk: function (code, chunk, outputOptions) {
-            writeHash(chunk.fileName, outputOptions.dir, code, options);
-        },
-        writeBundle: function (bundle) {
-            // console.log('writeBundle', bundle);
+            const filePath = options.formatter(outputOptions.dir.split(path.sep).join('/'));
+            const fileName = `${filePath}${chunk.fileName}`;
+
+            if (chunk.isEntry && !fileName.includes('.map')) {
+                addToManifest(fileName, code, options);
+            }
         },
     };
 }
