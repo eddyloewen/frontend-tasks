8708d6634574f50b4899441c8127f0af346577b2
Eddy Löwen <mail@eddyloewen.com>
2020-05-06 19:19:55 +0200

    removes rollup warnings during bundling because of external modules

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,31 @@
+const externals = [
+    'gulp',
+    'stylelint',
+    'gulp-postcss',
+    'gulp-flatten',
+    'merge-stream',
+    'gulp-plumber',
+    'gulp-sourcemaps',
+    'gulp-notify',
+    'gulp-if',
+    'gulp-environments',
+    'gulp-eslint',
+    'gulp-svg-sprite',
+    'path',
+    'through2',
+    'rollup',
+    'rollup-plugin-node-resolve',
+    'rollup-plugin-babel',
+    'rollup-plugin-terser',
+    'rollup-plugin-cleanup',
+    'rollup-plugin-postcss',
+    'rollup-plugin-commonjs',
+];
+
 module.exports = [
     {
         input: 'index.js',
+        external: externals,
         output: {
             interop: false,
             file: 'dist/cjs/index.js',
@@ -9,6 +34,7 @@ module.exports = [
     },
     {
         input: 'index.js',
+        external: externals,
         output: {
             interop: false,
             file: 'dist/es/index.js',
